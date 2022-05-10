e3bd8c88843be99dfea497d8763d16a7875532d7
soykot2910 <md.soykot2910@gmail.com>
2022-05-10 21:08:30 +0600

    readme add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import { Spinner } from "./components/SVGIcons";
+const Home = lazy(() => import("./pages/home"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="*" element={<h2>No Page Found</h2>} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="*"
+            element={
+              <div className="h-screen w-screen flex items-center justify-center">
+                <h2 className="text-6xl">Page Not Found</h2>
+              </div>
+            }
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
