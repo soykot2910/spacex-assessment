55f2cbcebc143007218eeeb104520d13cbe3a85d
soykot2910 <md.soykot2910@gmail.com>
2022-05-10 09:39:11 +0600

    basic configuration add

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        xl: "0rem",
+      },
+      center: true,
+      screens: {
+        default: "100%",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1170px",
+      },
+    },
+    fontFamily: {},
+  },
+  plugins: [],
+};
